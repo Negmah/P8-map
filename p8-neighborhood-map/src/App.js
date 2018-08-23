1372f264abc27fccb0eb1e5c1a1700f4c8977cb6
Monica Moura <monica.a.moura@hotmail.com>
2018-08-23 18:01:09 +0100

    Import and add Header component

--- p8-neighborhood-map/src/App.js
+++ p8-neighborhood-map/src/App.js
@@ -8,6 +8,7 @@
 
 import React, { Component } from 'react';
 import './App.css';
+import Header from './Header'
 
 
 
@@ -15,7 +16,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Header />
       </div>
     );
   }
