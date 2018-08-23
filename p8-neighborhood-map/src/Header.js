77d38d2703b22b55230efd99d3ba56e695dd4984
Monica Moura <monica.a.moura@hotmail.com>
2018-08-23 20:48:48 +0100

    Style Header and hamburger icon

--- p8-neighborhood-map/src/Header.js
+++ p8-neighborhood-map/src/Header.js
@@ -6,10 +6,14 @@ import './App.css';
 const Header = (props) => {
     return (
         <div className='header-wrapper' title='Pena Palace at night'>
-            <h1 className='header-title'>Mystical Sintra</h1>
-            <h4 className='header-footer'>#discoverPortugal</h4>
-            {/* Code partially taken from W3Schools */}
-            <span className='openbtn' onClick={props.navBar}>&#9776; open</span>
+            <div className='nav-menu'>
+                {/* Code partially taken from W3Schools */}
+                <div className='openbtn' onClick={props.navBar}>&#9776;</div>
+            </div>
+            <div className='header-text'>
+                <h1 className='header-title'>Mystical Sintra</h1>
+                <h4 className='header-footer'>#discoverPortugal</h4>    
+            </div>
         </div>
     );
 }
