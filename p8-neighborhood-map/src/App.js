fbbae756b7ffdfbf73479ba981b36a4db6cef005
Monica Moura <monica.a.moura@hotmail.com>
2018-08-23 17:56:18 +0100

    Add component description

--- p8-neighborhood-map/src/App.js
+++ p8-neighborhood-map/src/App.js
@@ -1,4 +1,11 @@
-import React, { Component } from 'react';
+/**
+ * @description FEND Capstone Project (P8) : Neighborhood Map
+ * @description main component
+ * @author Monica Moura
+ * @version 2.0
+ */
+
+ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
