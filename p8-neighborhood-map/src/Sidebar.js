6c003dd9f386826262cc13e9a98e701f1d056cf3
Monica Moura <monica.a.moura@hotmail.com>
2018-08-26 16:12:12 +0100

    Style sidebar

--- p8-neighborhood-map/src/Sidebar.js
+++ p8-neighborhood-map/src/Sidebar.js
@@ -2,14 +2,20 @@ import React, { Component } from "react";
 
 
 class Sidebar extends Component {
-  console.log('my props',props.venues)
+
+  state = {
+    query = ''
+  }
+ 
   render() {
+    console.log('my props', this.props.venues)
+
     return (
       //Code partially taken from W3Schools
       <nav id="navbar" className="sidebar">
         <div id='sidebar-header'>
           <img className='sidebar-icon' src='http://res.cloudinary.com/negmah/image/upload/v1535134022/pena-palace-icon.jpg' alt='Pena Palace icon'></img>
-          <button className='close-button' onClick={props.closeNavbar}>x</button> {/*implicit ARIA role: 'button'*/}
+          <button className='close-button' onClick={this.props.closeNavbar}>x</button> {/*implicit ARIA role: 'button'*/}
           {/*<a href='javascript:void(0)' className='close-button' onClick={props.closeNavbar}>&times;</a>*/}
         </div>
         <div>
@@ -17,7 +23,7 @@ class Sidebar extends Component {
         </div>
         <nav id='sidebar-list'>
           <ul>
-            {props.venues.map((venus) => 
+            {this.props.venues.map((venus) => 
               <li key={venus.venue.id}><a href='#'>{venus.venue.name}</a></li>
             )}
           </ul>
