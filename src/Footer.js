f24218985d599d80aa0a45337792253214766b5b
Monica Moura <monica.a.moura@hotmail.com>
2018-08-27 17:54:25 +0100

    Add credits

--- src/Footer.js
+++ src/Footer.js
@@ -7,7 +7,15 @@ const Footer = (props) => {
     return (
         <div id='footer-wrapper'>
             <footer id='footer'>
-                <i>It does not matter how slowly you go as long as you do not stop. Confucius</i>
+                <span id='quote'>
+                    <i>It does not matter how slowly you go as long as you do not stop. Confucius</i>
+                </span>
+                <div className='footer-text'>
+                        <span className='copyright'>
+                        Coded with love, sweat and tears for Udacity by &copy;
+                        <a target='_blank' rel="noopener noreferrer" href='https://www.linkedin.com/in/monica-moura-39024046/'>Monica Moura</a> {/*implicit ARIA role: 'link'*/}
+                        </span>
+                </div>
             </footer>
         </div>
     );
